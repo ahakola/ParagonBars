7917aa9d94cfce3492f1160817e9f9773b5bf150
Unknown <40722602+ahakola@users.noreply.github.com>
2022-10-29 22:28:15 +0300

    Dragonflight fixes

--- ParagonBars.lua
+++ ParagonBars.lua
@@ -8,40 +8,34 @@
 	mouse over progressbar like they show on uncapped reputations.
 ----------------------------------------------------------------------------]]--
 
-local color = { r = 0.26, g = 0.42, b = 1 } -- GameTooltipTemplate.xml#497
+local color = { r = 0.26, g = 0.42, b = 1 } -- https://www.townlong-yak.com/framexml/7.2.0/GameTooltipTemplate.xml#497
 
-local function hook_ReputationFrame_Update(...) -- ReputationFrame.lua#123
-	local numFactions = GetNumFactions()
-	local factionOffset = FauxScrollFrame_GetOffset(ReputationListScrollFrame)
+local function hook_ReputationFrame_Update(factionRow, elementData)
+	local factionIndex = elementData.index
+	local factionContainer = factionRow.Container
+	local factionBar = factionContainer.ReputationBar
 
-	for i = 1, NUM_FACTIONS_DISPLAYED do
-		local factionIndex = factionOffset + i
-		local factionRow = _G["ReputationBar"..i]
-		local factionBar = _G["ReputationBar"..i.."ReputationBar"]
+	local _, _, standingID, _, _, _, _, _, _, _, _, _, _, factionID = GetFactionInfo(factionIndex)
 
-		if factionIndex <= numFactions then
-			local _, _, standingID, _, _, _, _, _, _, _, _, _, _, factionID = GetFactionInfo(factionIndex)
+	if not standingID == MAX_REPUTATION_REACTION then return end
 
-			if not standingID == MAX_REPUTATION_REACTION then return end
-
-			if factionID and C_Reputation.IsFactionParagon(factionID) then
-				local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
-				if currentValue then
-					local value = mod(currentValue, threshold)
-					if hasRewardPending then
-						value = value + threshold
-					end
-					factionRow.rolloverText = HIGHLIGHT_FONT_COLOR_CODE.." "..format(REPUTATION_PROGRESS_FORMAT, BreakUpLargeNumbers(value), BreakUpLargeNumbers(threshold))..FONT_COLOR_CODE_CLOSE
-
-					factionBar:SetMinMaxValues(0, threshold)
-					factionBar:SetValue(value)
-					factionBar:SetStatusBarColor(color.r, color.g, color.b)
-				end
+	if factionID and C_Reputation.IsFactionParagon(factionID) then
+		local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
+		if currentValue then
+			local value = mod(currentValue, threshold)
+			if hasRewardPending then
+				value = value + threshold
 			end
+			factionRow.rolloverText = HIGHLIGHT_FONT_COLOR_CODE.." "..format(REPUTATION_PROGRESS_FORMAT, BreakUpLargeNumbers(value), BreakUpLargeNumbers(threshold))..FONT_COLOR_CODE_CLOSE
+
+			factionBar:SetMinMaxValues(0, threshold)
+			factionBar:SetValue(value)
+			factionBar:SetStatusBarColor(color.r, color.g, color.b)
 		end
 	end
 end
 
-hooksecurefunc("ReputationFrame_Update", hook_ReputationFrame_Update)
+--hooksecurefunc("ReputationFrame_Update", hook_ReputationFrame_Update) -- https://www.townlong-yak.com/framexml/7.2.0/ReputationFrame.lua#123
+hooksecurefunc("ReputationFrame_InitReputationRow", hook_ReputationFrame_Update) -- https://www.townlong-yak.com/framexml/10.0.0/ReputationFrame.lua#129
 
 --EOF
