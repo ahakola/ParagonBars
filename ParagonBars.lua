61d2050eb85674b738ba83ae5251d0852b02691e
ahakola <40722602+ahakola@users.noreply.github.com>
2024-08-21 00:00:59 +0300

    Fix addon for TWW

--- ParagonBars.lua
+++ ParagonBars.lua
@@ -10,6 +10,7 @@
 
 local color = { r = 0.26, g = 0.42, b = 1 } -- https://www.townlong-yak.com/framexml/7.2.0/GameTooltipTemplate.xml#497
 
+--[[
 local function hook_ReputationFrame_Update(factionRow, elementData)
 	local factionIndex = elementData.index
 	local factionContainer = factionRow.Container
@@ -34,7 +35,34 @@ local function hook_ReputationFrame_Update(factionRow, elementData)
 		end
 	end
 end
+]]
+local function hook_TWW_ReputationFrame_Update(self, ...)
+	local childFrames = { self.ScrollTarget:GetChildren() }
+	for i = 1, #childFrames do
+		if childFrames[i] and (not childFrames[i].elementData.isHeader) then -- Not all childFrames have ReputationBar in them, but all with a child named "Content" have it
+			local factionID = childFrames[i].factionID -- https://www.townlong-yak.com/framexml/11.0.2/Blizzard_UIPanels_Game/ReputationFrame.lua#208
+			local factionBar = childFrames[i].Content.ReputationBar -- https://www.townlong-yak.com/framexml/11.0.2/Blizzard_UIPanels_Game/ReputationFrame.lua#308
 
-hooksecurefunc("ReputationFrame_InitReputationRow", hook_ReputationFrame_Update) -- https://www.townlong-yak.com/framexml/10.0.0/ReputationFrame.lua#129
+			if factionID and C_Reputation.IsFactionParagon(factionID) then
+				local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
+
+				if currentValue then
+					local value = mod(currentValue, threshold)
+					if hasRewardPending then
+						value = value + threshold
+					end
+
+					-- https://www.townlong-yak.com/framexml/11.0.2/Blizzard_UIPanels_Game/ReputationFrame.lua#656 / 665 / 662
+					factionBar:UpdateBarValues(0, threshold, value)
+					factionBar:UpdateBarProgressText(HIGHLIGHT_FONT_COLOR:WrapTextInColorCode(REPUTATION_PROGRESS_FORMAT:format(BreakUpLargeNumbers(value), BreakUpLargeNumbers(threshold))))
+					factionBar:SetStatusBarColor(color.r, color.g, color.b)
+				end
+			end
+		end
+	end
+end
+
+--hooksecurefunc("ReputationFrame_InitReputationRow", hook_ReputationFrame_Update) -- https://www.townlong-yak.com/framexml/10.0.0/ReputationFrame.lua#129
+hooksecurefunc(ReputationFrame.ScrollBox, "Update", hook_TWW_ReputationFrame_Update) -- If we don't hook to ScrollBox, the bar re-coloring gets borked when scrolling
 
 --EOF
